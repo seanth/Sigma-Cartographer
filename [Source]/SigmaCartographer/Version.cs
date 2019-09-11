9563a92413e06df3781bf34236a75cd18c3f734c
seanth <seanth@gmail.com>
2019-09-11 12:07:56 -0500

    Update Version.cs
    version bumb

--- [Source]/SigmaCartographer/Version.cs
+++ [Source]/SigmaCartographer/Version.cs
@@ -6,7 +6,7 @@ namespace PsiCartographerPlugin
     [KSPAddon(KSPAddon.Startup.Instantly, true)]
     public class Version : MonoBehaviour
     {
-        public static readonly System.Version number = new System.Version("0.4.0");
+        public static readonly System.Version number = new System.Version("0.4.1");
 
         void Awake()
         {
